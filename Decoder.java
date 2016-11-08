336faf85bd2c7eb3540f61649aae19077b9d7c5d
waynezhou99 <waynez99@gmail.com>
2016-11-08 14:49:05 -0800

    1st checkin

---
+++ Decoder.java
@@ -0,0 +1,40 @@
+package com.fpd.decoder;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+public class Decoder implements Runnable
+{
+	
+	public static void main(String[] ap) 
+	{
+	    // schedule this for the event dispatch thread (edt)
+	    SwingUtilities.invokeLater(new Decoder());
+	}
+
+	public void run()
+	{
+		try
+		{
+			// Set the L&F for the Decoder to the system L&F.
+			try
+			{
+				UIManager.setLookAndFeel(
+				    UIManager.getSystemLookAndFeelClassName());
+			} 
+			catch (Exception e) 
+			{
+				// Strange if we can't set the L&F but the Decoder
+				// will run anyway.
+				e.printStackTrace();
+			}
+			
+			new Manager();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+}
